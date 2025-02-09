a4ae4d5056452e1fe6247ec368240a1a0fe152ef
Z1rconium <chenqiantargaryen@outlook.com>
2025-02-09 02:17:40 +0000

    Site updated: 2025-02-09 02:17:38

--- js/txmap.js
+++ js/txmap.js
@@ -29,7 +29,7 @@ function getDistance(e1, n1, e2, n2) {
 
 function showWelcome() {
 
-    let dist = getDistance(120.2645, 30.1851, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里换成自己的经纬度
+    let dist = getDistance(120.26, 30.30, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里换成自己的经纬度
     let pos = ipLoacation.result.ad_info.nation;
     let ip;
     let posdesc;
