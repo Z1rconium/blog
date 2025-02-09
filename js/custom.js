ca7bb137823a4d0771addc8c09fc652b7fe0ca49
Z1rconium <chenqiantargaryen@outlook.com>
2025-02-09 12:57:36 +0000

    Site updated: 2025-02-09 12:57:35

--- js/custom.js
+++ js/custom.js
@@ -1,44 +1,6 @@
-setInterval(() => {
-    // let create_time = Math.round(new Date('2021-10-15 00:00:00').getTime() / 1000); //在此行修改建站时间
-    // 有苹果用户发现safari浏览器不能正常运行，百度了一下发现是格式化的问题，改成下面这种应该就可以了。感谢反馈。
-    let create_time = Math.round(new Date('2025/2/1 21:00:00').getTime() / 1000); //在此行修改建站时间
-    let timestamp = Math.round((new Date().getTime()) / 1000);
-    let second = timestamp - create_time;
-    let time = new Array(0, 0, 0, 0, 0);
-
-    var nol = function(h) {
-        return h > 9 ? h : '0' + h;
-    }
-    if (second >= 365 * 24 * 3600) {
-        time[0] = parseInt(second / (365 * 24 * 3600));
-        second %= 365 * 24 * 3600;
-    }
-    if (second >= 24 * 3600) {
-        time[1] = parseInt(second / (24 * 3600));
-        second %= 24 * 3600;
-    }
-    if (second >= 3600) {
-        time[2] = nol(parseInt(second / 3600));
-        second %= 3600;
-    }
-    if (second >= 60) {
-        time[3] = nol(parseInt(second / 60));
-        second %= 60;
-    }
-    if (second >= 0) {
-        time[4] = nol(second);
-    }
-    let currentTimeHtml = ""
-    if (time[0] != 0) {
-        currentTimeHtml += time[0] + ' YEAR '
-    }
-    currentTimeHtml += time[1] + ' DAYS ' + time[2] + ' HOURS ' + time[3] + ' MINS ' + time[4] + ' SECS ';
-    document.getElementById("runtime").innerHTML = currentTimeHtml;
-}, 1000);
-
 // 动态心跳，更改自己的名称
 $(document).ready(function(e){
-    $('.copyright').html('©2022 <i class="fa-fw fas fa-heartbeat card-announcement-animation cc_pointer"></i> By 偷掉月亮的阿硕');
+    $('.copyright').html('©2025 <i class="fa-fw fas fa-heartbeat card-announcement-animation cc_pointer"></i> By 偷掉月亮的阿硕');
 })
 
 $(document).ready(function(e){
@@ -49,7 +11,7 @@ $(document).ready(function(e){
 function show_date_time(){
 $('.framework-info').html('小破站已经安全运行<span id="span_dt_dt" style="color: #fff;"></span>');
 window.setTimeout("show_date_time()", 1000);
-BirthDay=new Date("1/2/2025 21:0:0");
+BirthDay=new Date("2/1/2025 21:0:0");
 today=new Date();
 timeold=(today.getTime()-BirthDay.getTime());
 sectimeold=timeold/1000
